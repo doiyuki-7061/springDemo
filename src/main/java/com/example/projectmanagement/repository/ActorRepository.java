e151f639aeac46eca3e6c1d2ac26304cd0ac6a29
土居勇輝 <yuki7061jfc@yahoo.co.jp>
2026-05-17 15:52:12 +0900

    Actor関連削除

--- src/main/java/com/example/projectmanagement/repository/ActorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectmanagement.repository;
-
-import com.example.projectmanagement.entity.Actor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActorRepository extends JpaRepository<Actor, Integer> {
-}
